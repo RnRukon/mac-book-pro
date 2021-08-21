6edc4e1b181c26e30ccf20a80a46e548f6740a56
Your Name <you@example.com>
2021-08-21 16:02:37 +0600

    Assignment js 5

---
+++ app.js
@@ -0,0 +1,172 @@
+
+
+function customize(product) {
+    /* ----------8GB unified Memory----------- */
+    if (product == '8GB') {
+        const extraMemoryCost = document.getElementById('extra-memory-cost');
+        extraMemoryCost.innerText = 0;
+
+        const extraStorageCost = document.getElementById('extra-storage-cost');
+        extraStorageCost.innerText = 0;
+    }
+
+    /* -----------16GB unified--------------- */
+    else if (product == '16GB') {
+        const extraMemoryCost = document.getElementById('extra-memory-cost');
+        extraMemoryCost.innerText = 180;
+    }
+
+    /* ----------256 BG SSD Storage------------- */
+    else if (product == '256GB') {
+        const extraStorageCost = document.getElementById('extra-storage-cost');
+        extraStorageCost.innerText = 0;
+    }
+    /* ----------512 BG SSD Storage------------- */
+    else if (product == "512gb") {
+        const extraStorageCost = document.getElementById('extra-storage-cost');
+        extraStorageCost.innerText = 100;
+    }
+    /* ----------1TB SSD Storage------------- */
+    else if (product == "1TB") {
+        const extraStorageCost = document.getElementById('extra-storage-cost');
+        extraStorageCost.innerText = 180;
+    }
+
+
+    /* ----------Friday.Aug 25 FREE Prime Delivery------------- */
+
+    else if (product == 'friday') {
+        const deliveryCharge = document.getElementById('delivery-charge');
+        deliveryCharge.innerText = 0;
+    }
+    /* ----------Friday.Aug 21  Delivery Charge $20------------- */
+    else if (product == 20) {
+        const deliveryCharge = document.getElementById('delivery-charge');
+        deliveryCharge.innerText = 20;
+
+    }
+
+}
+
+
+
+
+
+/* --------------------Total Price calculation----------------- */
+
+function totalPrice() {
+    const bestPrice = document.getElementById('best-price');
+    const bestPriceText = parseFloat(bestPrice.innerText);
+    const extraMemoryCost = document.getElementById('extra-memory-cost');
+    const extraMemoryCostText = parseFloat(extraMemoryCost.innerText);
+
+    const extraStorageCost = document.getElementById('extra-storage-cost');
+    const extraStorageCostText = parseFloat(extraStorageCost.innerText);
+
+    const deliveryCharge = document.getElementById('delivery-charge');
+    const deliveryChargeText = parseFloat(deliveryCharge.innerText);
+
+
+    const totalCostPrice = document.getElementById('total-price');
+
+    /*----------- Total ---------*/
+    const totalPriceCost = bestPriceText + extraMemoryCostText + extraStorageCostText + deliveryChargeText;
+
+    totalCostPrice.innerText = totalPriceCost;
+    const total = document.getElementById('total');
+    total.innerText = totalPriceCost;
+
+
+};
+
+
+
+
+/*----------------- percentage calculation--------------------*/
+
+
+function discount() {
+
+    const promoCodeField = document.getElementById('promo-code-field');
+    const promoCodeFieldValue = promoCodeField.value;
+
+
+    const total = document.getElementById('total');
+    const totalText = parseInt(total.innerText);
+
+
+    if (promoCodeFieldValue == 'stevekaku') {
+
+        const discount = (totalText / 100) * 20;
+
+        const totalDiscount = totalText - discount;
+        total.innerText = totalDiscount;
+        promoCodeField.value = "";
+    }
+};
+
+
+
+/*------------- 8GB unified memory button --------------*/
+document.getElementById('memory-unified-8Gb-Btn').addEventListener('click', function () {
+    customize('8GB');
+    totalPrice();
+    discount();
+});
+
+/*------------- 16GB unified memory button----------------*/
+document.getElementById('memory-unified-16GB-Btn').addEventListener('click', function () {
+
+    // customizeMemory16gb();
+    customize('16GB');
+    totalPrice();
+    discount();
+});
+
+/* ---------------256GB SSD storage button -----------------------*/
+document.getElementById('storage-ssd-256GB-Btn').addEventListener('click', function () {
+    // customizeMemory256gb('256GB');
+    customize('256GB');
+    totalPrice();
+    discount();
+});
+
+/*------------------ 512GB SSD storage button -----------------*/
+document.getElementById('storage-ssd-512GB-Btn').addEventListener('click', function () {
+    // customizeMemory512gb();
+    customize('512gb');
+    totalPrice();
+    discount();
+});
+
+
+/*-------------- 1TB SSD storage button -------------------*/
+document.getElementById('storage-ssd-1TB-Btn').addEventListener('click', function () {
+    // customizeMemory1tb()
+    customize("1TB");
+    totalPrice();
+    discount();
+});
+
+/*-------------- Friday,Aug 25 FREE Prime Delivery button -------------------*/
+document.getElementById('friday-Aug-25-free-delivery-Btn').addEventListener('click', function () {
+    // freeDay('friday');
+    customize('friday');
+    totalPrice();
+    discount();
+});
+
+/*-------------- Friday,Aug,21 Delivery charge $20 button -------------------*/
+document.getElementById('friday-agu-21-charge-btn').addEventListener('click', function () {
+    // chargeDay();
+    customize(20);
+    totalPrice();
+    discount();
+});
+
+/*-------------- Add Promo Code Apply button -------------------*/
+document.getElementById('promo-code-apply-btn').addEventListener('click', function () {
+    discount();
+});
+
+/* <<<<<<<<<<<<<<<<<<<<<END>>>>>>>>>>>>>>>>>>>> */
