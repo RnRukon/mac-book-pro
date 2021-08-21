47593532f96d4337d1996f180c673151df1188a5
Your Name <you@example.com>
2021-08-21 17:18:11 +0600

    commit

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
 
 
-function customize(product) {
+function customize(product, extra) {
+
+    const extraMemoryCost = document.getElementById(extra + '-memory-cost');
+    const extraStorageCost = document.getElementById(extra + '-storage-cost');
+
+
     /* ----------8GB unified Memory----------- */
     if (product == '8GB') {
-        const extraMemoryCost = document.getElementById('extra-memory-cost');
         extraMemoryCost.innerText = 0;
-
-        const extraStorageCost = document.getElementById('extra-storage-cost');
         extraStorageCost.innerText = 0;
     }
 
     /* -----------16GB unified--------------- */
     else if (product == '16GB') {
-        const extraMemoryCost = document.getElementById('extra-memory-cost');
         extraMemoryCost.innerText = 180;
     }
 
     /* ----------256 BG SSD Storage------------- */
     else if (product == '256GB') {
-        const extraStorageCost = document.getElementById('extra-storage-cost');
         extraStorageCost.innerText = 0;
     }
     /* ----------512 BG SSD Storage------------- */
     else if (product == "512gb") {
-        const extraStorageCost = document.getElementById('extra-storage-cost');
         extraStorageCost.innerText = 100;
     }
     /* ----------1TB SSD Storage------------- */
     else if (product == "1TB") {
-        const extraStorageCost = document.getElementById('extra-storage-cost');
+
         extraStorageCost.innerText = 180;
     }
 
@@ -49,14 +48,12 @@ function customize(product) {
 }
 
 
-
-
-
 /* --------------------Total Price calculation----------------- */
 
 function totalPrice() {
     const bestPrice = document.getElementById('best-price');
     const bestPriceText = parseFloat(bestPrice.innerText);
+
     const extraMemoryCost = document.getElementById('extra-memory-cost');
     const extraMemoryCostText = parseFloat(extraMemoryCost.innerText);
 
@@ -109,32 +106,30 @@ function discount() {
 
 /*------------- 8GB unified memory button --------------*/
 document.getElementById('memory-unified-8Gb-Btn').addEventListener('click', function () {
-    customize('8GB');
+    customize('8GB', 'extra');
     totalPrice();
     discount();
 });
 
 /*------------- 16GB unified memory button----------------*/
 document.getElementById('memory-unified-16GB-Btn').addEventListener('click', function () {
-
-    // customizeMemory16gb();
-    customize('16GB');
+    customize('16GB', 'extra');
     totalPrice();
     discount();
 });
 
 /* ---------------256GB SSD storage button -----------------------*/
 document.getElementById('storage-ssd-256GB-Btn').addEventListener('click', function () {
-    // customizeMemory256gb('256GB');
-    customize('256GB');
+
+    customize('256GB', 'extra');
     totalPrice();
     discount();
 });
 
 /*------------------ 512GB SSD storage button -----------------*/
 document.getElementById('storage-ssd-512GB-Btn').addEventListener('click', function () {
-    // customizeMemory512gb();
-    customize('512gb');
+
+    customize('512gb', 'extra');
     totalPrice();
     discount();
 });
@@ -142,15 +137,14 @@ document.getElementById('storage-ssd-512GB-Btn').addEventListener('click', funct
 
 /*-------------- 1TB SSD storage button -------------------*/
 document.getElementById('storage-ssd-1TB-Btn').addEventListener('click', function () {
-    // customizeMemory1tb()
-    customize("1TB");
+    customize("1TB", 'extra');
     totalPrice();
     discount();
 });
 
 /*-------------- Friday,Aug 25 FREE Prime Delivery button -------------------*/
 document.getElementById('friday-Aug-25-free-delivery-Btn').addEventListener('click', function () {
-    // freeDay('friday');
+
     customize('friday');
     totalPrice();
     discount();
@@ -158,7 +152,6 @@ document.getElementById('friday-Aug-25-free-delivery-Btn').addEventListener('cli
 
 /*-------------- Friday,Aug,21 Delivery charge $20 button -------------------*/
 document.getElementById('friday-agu-21-charge-btn').addEventListener('click', function () {
-    // chargeDay();
     customize(20);
     totalPrice();
     discount();
